4e4fb7918cec1c6ec90a075257d5a52e2a5f4cd6
Lucas-OF28 <luuu_of@hotmail.com>
2022-07-20 14:09:39 -0300

    finalizando a sessão

---
+++ 4_estruturas/19_switch/scripts.js
@@ -0,0 +1,13 @@
+let nome = "Lucas"
+
+switch (nome){
+  case "Matheus":
+    console.log("O nome é Matheus")
+    break;
+  case "Lucas":
+    console.log("O nome é Lucas")
+    break;
+  default: 
+    console.log("Nome não encontrado")
+    break;
+}
